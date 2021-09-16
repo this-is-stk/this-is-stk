014e439cb56c57631fe2b81909787162cf26bc7d
this-is-stk <90714670+this-is-stk@users.noreply.github.com>
2021-09-16 14:28:31 +0100

    Update config_apache.sh
    Updated with new Webpage information

--- config_apache.sh
+++ config_apache.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-sleep 3m
+sleep 2m
 dpkg --configure -a
 apt-get -y update
 
-# install Apache2
+# install Apache2 on the newly deployed VM
 apt-get -y install apache2
 apt-get -y install mysql-server
 apt-get -y install php libapache2-mod-php php-mysql
-# write some HTML
-echo \<center\>\<h1\>This is sample NWaaP Portal\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# Create the webpage to be seem on launching the URL
+echo \<center\>\<h1\>Welcome to NWaaP Release 2 Integration and Verification\</h1\>\</center\> > /var/www/html/demo.html
+echo \<p\>\<b\>I\&V Team Members\:\</b\> Alan Lynam\, Dylan Loftus\, Santhosh George\, Srijna Saxena\, Subhashith T\.K\, Surendran Akambadiar \</p\> >> /var/www/html/demo.html
+echo \<center\>\<img src\=\"https\://www.ericsson.com/4aaaa2/assets/local/digital-services/offerings/network-automation/na-components.png\" width\=\"1100\" height\=\"700\"\>\</center\> >> /var/www/html/demo.html
+echo \<center\>\<img src\=\"https\://upload.wikimedia.org/wikipedia/commons/e/e9/Ericsson_logo.svg\" alt\=\"upload.wikimedia.org\" width\=\"104\" height\=\"142\"\>\</center\> >> /var/www/html/demo.html
 
 # restart Apache
 apachectl restart
